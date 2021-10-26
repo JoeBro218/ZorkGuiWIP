bd5eae4735536da7a296d64b03c07d84ea77fd5f
JoeBro218 <70218539+JoeBro218@users.noreply.github.com>
2021-10-25 20:04:27 -0400

    Zork Items

---
+++ Zork/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Zork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string DefaultGameFilename = "Zork.json";
+            string GameFilename = args.Length > 0 ? args[(int)CommandLineArgument.GameFilename] : DefaultGameFilename;
+
+            Game game = Game.Load(GameFilename);
+            game.Run();
+
+        }
+
+        private enum CommandLineArgument
+        {
+            GameFilename = 0
+        }
+    }
+}
