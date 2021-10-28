09198c210eaf9a19ab76608ba96d13f0f765ec8f
JoeBro218 <70218539+JoeBro218@users.noreply.github.com>
2021-10-27 21:42:44 -0400

    New additions to the GUI as well as code of the other world and room.

---
+++ Zork Builder/Zork Builder/UserControls/DirectionUserControl.Designer.cs
@@ -0,0 +1,70 @@
+﻿
+namespace Zork_Builder.UserControls
+{
+    partial class DirectionUserControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBox1.Location = new System.Drawing.Point(0, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(231, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(0, 29);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(231, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // DirectionUserControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "DirectionUserControl";
+            this.Size = new System.Drawing.Size(231, 53);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
--- Zork Builder/Zork.Common/World.cs
+++ Zork Builder/Zork.Common/World.cs
@@ -14,5 +14,29 @@ namespace Zork.Common
         {
             Rooms = new List<Room>(rooms);
         }
+
+        //---THIS IS THE CODE FROM THE ORIGONAL WORLD CLASS---//
+        //public HashSet<Room> Rooms { get; set; }
+
+        //[JsonIgnore]
+        //public Dictionary<string, Room> RoomsByName => mRoomsByName;
+
+        //public Player SpawnPlayer() => new Player(this, StartingLocation);
+
+        //[OnDeserialized]
+        //private void OnDeserialized(StreamingContext context)
+        //{
+        //    mRoomsByName = Rooms.ToDictionary(room => room.Name, room => room);
+
+        //    foreach (Room room in Rooms)
+        //    {
+        //        room.UpdateNeighbors(this);
+        //    }
+        //}
+
+        //[JsonProperty]
+        //private string StartingLocation { get; set; }
+
+        //private Dictionary<string, Room> mRoomsByName;
     }
 }
