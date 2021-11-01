320d39b42c659048716d764b4a59706567815ea6
JoeBro218 <70218539+JoeBro218@users.noreply.github.com>
2021-11-01 19:12:14 -0400

    Zork Update, comment a lot out

--- Zork Builder/Zork.Common/Game.cs
+++ Zork Builder/Zork.Common/Game.cs
@@ -6,108 +6,108 @@ namespace Zork
 {
     public class Game
     {
-        private int Moves = 0;
-        private int Score = 0;
-        public World World { get; set;}
+    //    private int Moves = 0;
+    //    private int Score = 0;
+    //    public World World { get; set;}
 
-        [JsonIgnore]
-        public Player Player { get; private set;}
+    //    [JsonIgnore]
+    //    public Player Player { get; private set;}
 
-        public string WelcomeMessage { get; set; }
+    //    public string WelcomeMessage { get; set; }
 
-        public string ExitMessage { get; set; }
+    //    public string ExitMessage { get; set; }
 
-        [JsonIgnore]
-        public bool IsRunning { get; set; }
+    //    [JsonIgnore]
+    //    public bool IsRunning { get; set; }
 
-        public Game(World world, Player player)
-        {
-            World = world;
-            Player = player;
-        }
+    //    public Game(World world, Player player)
+    //    {
+    //        World = world;
+    //        Player = player;
+    //    }
 
 
-        public void Run()
-        {
-            Console.WriteLine(WelcomeMessage);
+    //    public void Run()
+    //    {
+    //        Console.WriteLine(WelcomeMessage);
 
-            IsRunning = true;
-            Room previousRoom = null;
-            while (IsRunning)
-            {
-                Console.WriteLine(Player.Location);
-                if (previousRoom != Player.Location)
-                {
-                    Console.WriteLine(Player.Location.Description);
-                    previousRoom = Player.Location;
-                }
-                Console.Write("> ");
+    //        IsRunning = true;
+    //        Room previousRoom = null;
+    //        while (IsRunning)
+    //        {
+    //            Console.WriteLine(Player.Location);
+    //            if (previousRoom != Player.Location)
+    //            {
+    //                Console.WriteLine(Player.Location.Description);
+    //                previousRoom = Player.Location;
+    //            }
+    //            Console.Write("> ");
 
-                Commands command = ToCommand(Console.ReadLine().Trim());
+    //            Commands command = ToCommand(Console.ReadLine().Trim());
 
-                switch (command)
-                {
-                    case Commands.QUIT:
-                        IsRunning = false;
-                        Moves += 1;
-                        Console.WriteLine($"Your final score is {Score} and the amount of moves you made are {Moves}.");
-                        Console.WriteLine(ExitMessage);
-                        break;
+    //            switch (command)
+    //            {
+    //                case Commands.QUIT:
+    //                    IsRunning = false;
+    //                    Moves += 1;
+    //                    Console.WriteLine($"Your final score is {Score} and the amount of moves you made are {Moves}.");
+    //                    Console.WriteLine(ExitMessage);
+    //                    break;
 
-                    case Commands.LOOK:
-                        Console.WriteLine(Player.Location.Description);
-                        break;
+    //                case Commands.LOOK:
+    //                    Console.WriteLine(Player.Location.Description);
+    //                    break;
 
-                    case Commands.NORTH:
-                    case Commands.SOUTH:
-                    case Commands.EAST:
-                    case Commands.WEST:
-                        Console.WriteLine(Player.Move((Directions)command) ? $"You moved {command}." : "The way is shut.");
-                        Moves += 1;
-                        break;
+    //                case Commands.NORTH:
+    //                case Commands.SOUTH:
+    //                case Commands.EAST:
+    //                case Commands.WEST:
+    //                    Console.WriteLine(Player.Move((Directions)command) ? $"You moved {command}." : "The way is shut.");
+    //                    Moves += 1;
+    //                    break;
 
-                    case Commands.REWARD:
-                        Random RanNum = new Random();
-                        int Num = RanNum.Next(1, 11);
-                        string message = "place holder";
-                        if (Num == 1)
-                        {
-                            message = "A rock. It's worthless.";
-                        }
-                        else if (Num > 1 && Num < 10)
-                        {
-                            message = "Woah! Free Coin!";
-                            Score += 1;
-                        }
-                        else if (Num >= 10)
-                        {
-                            message = "Shinny Gold Egg!";
-                            Score += 10;
-                        }
-                        Moves += 1;
-                        Console.WriteLine(message);
-                        break;
+    //                case Commands.REWARD:
+    //                    Random RanNum = new Random();
+    //                    int Num = RanNum.Next(1, 11);
+    //                    string message = "place holder";
+    //                    if (Num == 1)
+    //                    {
+    //                        message = "A rock. It's worthless.";
+    //                    }
+    //                    else if (Num > 1 && Num < 10)
+    //                    {
+    //                        message = "Woah! Free Coin!";
+    //                        Score += 1;
+    //                    }
+    //                    else if (Num >= 10)
+    //                    {
+    //                        message = "Shinny Gold Egg!";
+    //                        Score += 10;
+    //                    }
+    //                    Moves += 1;
+    //                    Console.WriteLine(message);
+    //                    break;
 
-                    case Commands.SCORE:
-                        Moves += 1;
-                        Console.WriteLine($"Your score is {Score} and you have made {Moves} moves.");
-                        break;
+    //                case Commands.SCORE:
+    //                    Moves += 1;
+    //                    Console.WriteLine($"Your score is {Score} and you have made {Moves} moves.");
+    //                    break;
 
-                    default:
-                        Console.WriteLine("Unknown command.");
-                        break;
-                }
-            }
-        }
+    //                default:
+    //                    Console.WriteLine("Unknown command.");
+    //                    break;
+    //            }
+    //        }
+    //    }
 
-        public static Game Load(string filename)
-        {
-            Game game = JsonConvert.DeserializeObject<Game>(File.ReadAllText(filename));
-            game.Player = game.World.SpawnPlayer();
+    //    public static Game Load(string filename)
+    //    {
+    //        Game game = JsonConvert.DeserializeObject<Game>(File.ReadAllText(filename));
+    //        game.Player = game.World.SpawnPlayer();
             
-            return game;
-        }
+    //        return game;
+    //    }
 
-        private static Commands ToCommand(string commandString) => Enum.TryParse<Commands>(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
+    //    private static Commands ToCommand(string commandString) => Enum.TryParse<Commands>(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
     }
 }
--- Zork Builder/Zork.Common/Player.cs
+++ Zork Builder/Zork.Common/Player.cs
@@ -3,42 +3,42 @@ using System.Collections.Generic;
 
 namespace Zork
 {
-    public class Player
-    {
-        public World World { get; }
+    //public class Player
+    //{
+    //    public World World { get; }
         
-        [JsonIgnore]
-        public Room Location { get; set; }
+    //    [JsonIgnore]
+    //    public Room Location { get; set; }
 
-        [JsonIgnore]
-        public string LocationName
-        {
-            get
-            {
-                return Location?.Name;
-            }
-            set
-            {
-                Location = World?.RoomsByName.GetValueOrDefault(value);
-            }
-        }
+    //    [JsonIgnore]
+    //    public string LocationName
+    //    {
+    //        get
+    //        {
+    //            return Location?.Name;
+    //        }
+    //        set
+    //        {
+    //            Location = World?.RoomsByName.GetValueOrDefault(value);
+    //        }
+    //    }
 
-        public Player(World world, string startinglocation)
-        {
-            World = world;
+    //    public Player(World world, string startinglocation)
+    //    {
+    //        World = world;
 
-            LocationName = startinglocation;
-        }
+    //        LocationName = startinglocation;
+    //    }
 
-        public bool Move(Directions direction)
-        {
-            bool IsVaildMove = Location.Neighbors.TryGetValue(direction, out Room neighbor);
-            if (IsVaildMove)
-            {
-                Location = neighbor;
-            }
+    //    public bool Move(Directions direction)
+    //    {
+    //        bool IsVaildMove = Location.Neighbors.TryGetValue(direction, out Room neighbor);
+    //        if (IsVaildMove)
+    //        {
+    //            Location = neighbor;
+    //        }
 
-            return IsVaildMove;
-        }
-    }
+    //        return IsVaildMove;
+    //    }
+    //}
 }
